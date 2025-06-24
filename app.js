0fcd1e6c28c20265528e9cc8eb3414b9a73604e8
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2025-06-24 19:57:40 +0530

    HTTP Handling

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.get("/", (req, res) => {
 app.use('/user', router);
 
 
+app.post("/users", express.json(), (req, res) => {
+  const  { name, email } = req.body;
+  res.json({
+    message: `User ${name} with email ${email} created successfully!`
+  })
+})
+
+
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port http://localhost:3000");
 });
