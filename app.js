f8e63f1dcb521cadf2bcf14e7e250dee374ad412
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2025-07-02 16:17:33 +0530

    Middleware

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.json());
 
 
+// Middleware
+app.use((req, res, next) => {
+    console.log("A new request received at " + Date.now());
+    next();
+})
+
 
 // Handling a GET Request
 app.get("/", (req, res) => {
@@ -18,34 +24,34 @@ app.get("/", (req, res) => {
 
 
 // Handling a POST Request
-app.post("/users", (req, res) => {
-  const  { name, email } = req.body;
-  res.json({
-    message: `User ${name} with email ${email} created successfully!`
-  })
-})
+// app.post("/users", (req, res) => {
+//   const  { name, email } = req.body;
+//   res.json({
+//     message: `User ${name} with email ${email} created successfully!`
+//   })
+// })
 
 
 
-// Handling a PUT Request
-app.put("/users/:id", (req,res) => {
-  const userId = req.params.id;
-  const {name, email} = req.body;
-  res.json({
-    message: `User with ID ${userId} updated to ${name}, ${email} successfully!`,
-  });
-});
+// // Handling a PUT Request
+// app.put("/users/:id", (req,res) => {
+//   const userId = req.params.id;
+//   const {name, email} = req.body;
+//   res.json({
+//     message: `User with ID ${userId} updated to ${name}, ${email} successfully!`,
+//   });
+// });
 
 
 
 
 // Handling a DELETE Request
-app.delete("/users/:id", (req, res) => {
-  const userId = req.params.id;
-  res.json({
-    message: `User with ID ${userId} deleted successfully!`
-  });
-});
+// app.delete("/users/:id", (req, res) => {
+//   const userId = req.params.id;
+//   res.json({
+//     message: `User with ID ${userId} deleted successfully!`
+//   });
+// });
 
 
 app.listen(3000, () => {
