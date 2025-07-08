b9da09372742e1f03dca2072ef8a76a0c779ae1e
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2025-07-08 16:17:45 +0530

    Form Data Handling

--- form.js
+++ form.js
@@ -1,40 +1,47 @@
-const express = require("express");
-const multer = require("multer");
+const express = require('express');
+const multer = require('multer');
+
+
 
 
 const storage = multer.diskStorage({
-    destination: 'uploads',
+    destination: "uploads",
     filename: (req, file, callBack) => {
-        callBack(null, file.fieldname+'_'+Date.now()+file.originalname);
+        callBack(null, file.fieldname+"_"+Date.now()+file.originalname);
     }
-});
+})
+
 
 
 const app = express();
 // const upload = multer();
 const upload = multer({
     storage,
-    limits: { fileSize: 1000000 }, // Limit file size to 1MB
-});
+    limits: { fileSize: 1000000 },
+})
+
 
 
 app.use(express.urlencoded({ extended: true }));
 // app.use(upload.array());
-app.use(upload.single('image'));
+app.use(upload.single("image"));
+
 
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Home Page!");
-});
-
-
-app.post("/form", (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
-  res.send("Form data received successfully!");
 })
 
 
+
+app.post("/form", (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+    res.send("Form Data Received!");
+})
+
+
+
 app.listen(3000, () => {
-  console.log("Server is running on port http://localhost:3000");
-});
+    console.log("Server is running on port http://localhost:3000");
+})
