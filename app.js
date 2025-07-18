4a30c13ea464991bc7e8e74ebea7f6ba5e52409c
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2025-07-18 16:58:59 +0530

    RESTful APIs

--- app.js
+++ app.js
@@ -327,8 +327,6 @@
 // });
 
 
-
-
 // app.post("/login", (req, res) => {
 //     const { username, password } = req.body;
 //     const user = users.find(u => u.username === username);
@@ -340,15 +338,12 @@
 // });
 
 
-
 // app.get("/profile", (req, res) => {
 //     if(!req.session.user){
 //         return res.send("You are unauthorized person");
 //     }
 //     res.send(`Welcome to your profile, ${req.session.user.username}!`);
 // })
-
-
 
 
 // app.listen(3000, () => {
@@ -365,60 +360,137 @@
 // 2. Token-Based Authentication
 // (Uses JWT or OAuth tokens)
 
-import express from 'express';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+// import express from "express";
+// import jwt from "jsonwebtoken";
+// import bcrypt from "bcryptjs";
+
+
+
+// const app = express();
+
+// app.use(express.json());
+
+// const users = [];
+
+// app.get("/", (req, res) => {
+//     res.send("Welcome to the Home Page!");
+// });
+
+
+// app.post("/signup", async (req, res) => {
+//     const { username, password } = req.body;
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     users.push({
+//         username,
+//         password: hashedPassword
+//     });
+//     res.send("User signed up successfully!");
+// });
+
+
+// app.post("/login", async (req, res) => {
+//     const { username, password } = req.body;
+//     const user = users.find(u => u.username === username);
+//     if(!user || !(await bcrypt.compare(password, user.password))) {
+//         return res.send("Invalid username or password");
+//     }
+//     const token = jwt.sign({username}, "test#secret");
+//     res.json({token});
+// });
+
+
+// app.get("/profile", (req, res) => {
+//     try{
+//         const token = req.header("authorization");
+//         const decodedToken = jwt.verify(token, "test#secret");
+//         if(!decodedToken.username){
+//             res.send(`Welcome, ${decodedToken.username}`);
+//         } else {
+//             res.send("Welcome to your profile!");
+//         }
+//     } catch(err) {
+//         return res.send("Unauthorized");
+//     }
+// })
+
+
+// app.listen(3000, () => {
+//   console.log("Server is running on http://localhost:3000");
+// });
+
+
+
+
+
+
+
+
+
+
+// Building a RESTful API with Express.js
+
+import express from "express";
 
 
 const app = express();
 app.use(express.json());
-
-const users = [];
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Home Page!");
 });
 
 
-app.post("/signup", async (req, res) => {
-    const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    users.push({
-        username,
-        password: hashedPassword
-    });
-    res.send("User signed up successfully!");
+// GET all Products
+
+app.get("/api/products", (req, res) => {
+    const products = [
+        { id: 1, name: "Laptop", price: 50000, description: "Acer Aspire 3 A315-24P-R7VH Slim Laptop | 15.6 Full HD  AMD Ryzen 3 7320U Quad-Core | AMD Radeon Graphics | 8GB LPDDR5 | 128GB NVMe SSD | Wi-Fi 6 | Windows 11 Home", image: "https://i.pinimg.com/736x/7b/b8/12/7bb812007991708f413fb44c36f4ffd1.jpg" },
+        { id: 2, name: "Mobile", price: 25000 },
+        { id: 3, name: "iPad", price: 75000 },
+        { id: 4, name: "Smart Watch", price: 15000 },
+        { id: 5, name: "Headphones", price: 5000 },
+        { id: 6, name: "Bluetooth Speaker", price: 3000 },
+        { id: 7, name: "Camera", price: 40000 },
+        { id: 8, name: "Gaming Console", price: 60000 },
+        { id: 9, name: "Monitor", price: 20000 },
+        { id: 10, name: "Keyboard", price: 2000 }
+    ]
+    res.status(200).json({products});
 });
 
 
 
+// GET a single Product
 
-app.post("/login", async (req, res) => {
-    const { username, password } = req.body;
-    const user = users.find(u => u.username === username);
-    if(!user || !(await bcrypt.compare(password, user.password))) {
-        return res.send("Invalid username or password");
+app.get("/api/products/:id", (req, res) => {
+    const products = [
+        { id: 1, name: "Laptop", price: 50000, },
+        { id: 2, name: "Mobile", price: 25000 },
+        { id: 3, name: "iPad", price: 75000 },
+        { id: 4, name: "Smart Watch", price: 15000 },
+        { id: 5, name: "Headphones", price: 5000 },
+        { id: 6, name: "Bluetooth Speaker", price: 3000 },
+        { id: 7, name: "Camera", price: 40000 },
+        { id: 8, name: "Gaming Console", price: 60000 },
+        { id: 9, name: "Monitor", price: 20000 },
+        { id: 10, name: "Keyboard", price: 2000 }
+    ]
+    const product = products.find(p => p.id === Number(req.params.id));
+    if(!product){
+        return res.status(404).json({message: "Product not found"});
     }
-    const token = jwt.sign({username}, "test#secret");
-    res.json({token});
+    res.status(200).json({product});
 });
 
 
 
-app.get("/profile", (req, res) => {
-    try {
-        const token = req.header("authorization");
-    const decodedToken = jwt.verify(token, "test#secret");
-    if(!decodedToken.username) {
-        res.send(`Welcome, ${decodedToken.username}!`);
-    } else {
-        res.send("Welcome to your profile");
-    } 
-    } catch (error) {
-        res.send("Unauthorized access");
-    }
-})
+// Create a new Product
 
+app.post("/api/products", (req, res) => {
+    const newProduct = req.body;
+    newProduct.id = Date.now();
+    res.status(201).json(newProduct);
+});
 
 
 
